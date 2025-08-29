b06c29eb90e6a8136b8c43868b164a57300e4f16
Alex Oliveira <iamsralex@gmail.com>
2025-08-29 20:23:37 -0300

    Atualizando a main

--- database/tables/main.py
+++ database/tables/main.py
@@ -3,25 +3,30 @@ from config.database import *
 
 
 def main():
-    # Inicia a conexão
-    dados = GenerateData(DB_NAME, DB_PATH, DB_OWNER, DB_HOST, DB_PASSWORD)
-    dados.initialize()
-    
-    # Gera os dados
-    dados.generate_itens(N_ITENS)
-    dados.generate_clientes(N_CLIENTES, MAX_NUM_TELEFONE, PROB_ENFERMIDADE)
-    dados.generate_ingredientes(N_INGREDIENTES)
-    dados.generate_filiais(N_FILIAIS)
-    dados.generate_fornecedores(N_FORNECEDORES)
-    dados.generate_funcionarios(N_FUNCIONARIOS)
-    dados.generate_pedido_pedidoitem(N_PEDIDOS, MAX_QTD)
-    dados.generate_itemingrediente(MAX_INGREDIENTES)
-    dados.generate_reservas(N_RESERVAS, MAX_MESAS)
-    dados.generate_pedidos(N_PEDIDOS, MAX_QTD, MAX_ITENS)
-    
-    # Finaliza as operações
-    dados.commit()
-    dados.close()
+    try:
+        # Inicia a conexão
+        dados = GenerateData(DB_NAME, DB_PATH, DB_OWNER, DB_HOST, DB_PASSWORD)
+        dados.initialize()
+        
+        # Gera os dados
+        dados.generate_itens(N_ITENS)
+        dados.generate_clientes(N_CLIENTES, MAX_NUM_TELEFONE, PROB_ENFERMIDADE)
+        dados.generate_ingredientes(N_INGREDIENTES)
+        dados.generate_filiais(N_FILIAIS)
+        dados.generate_fornecedores(N_FORNECEDORES)
+        dados.generate_funcionarios(N_FUNCIONARIOS)
+        dados.generate_pedido_pedidoitem(N_PEDIDOS, MAX_QTD)
+        dados.generate_itemingrediente(MAX_INGREDIENTES)
+        dados.generate_reservas(N_RESERVAS, MAX_MESAS)
+        dados.generate_pedidos(N_PEDIDOS, MAX_QTD, MAX_ITENS)
+        
+        # Finaliza as operações
+        dados.commit()
+        dados.close()
+        print("Dados gerados com sucesso!")
+
+    except Exception as e:
+        print("Ocorreu um erro", e)
 
 
 if __name__ == "__main__":
