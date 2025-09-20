7afbb1fbe97db21d2afe0bdbb9dfed227856da53
jaopredo <joao.profissional.45@gmail.com>
2025-09-20 18:01:37 -0300

    Inserindo os itens e os calendários nas dimensões

--- sql/ETL_carga_inicial.sql
+++ sql/ETL_carga_inicial.sql
@@ -10,7 +10,8 @@ INSERT INTO dw_cho.FilialDimension SELECT
     f.FilialEstado
 FROM cho.Filiais f;
 
-INSERT INTO dw.ClienteDimension SELECT
+-- Inserindo os Clientes
+INSERT INTO dw_cho.ClienteDimension SELECT
     gen_random_uuid(),
     c.ClienteCPF,
     c.ClienteNome,
@@ -23,3 +24,40 @@ INSERT INTO dw.ClienteDimension SELECT
     c.ClienteDataNasc,
     cce.ClienteEnfermidade
 FROM cho.Clientes c LEFT JOIN cho.ClienteClienteEnfermidade cce ON c.ClienteCPF=cce.ClienteCPF;
+
+-- Inserindo os Itens
+INSERT INTO dw_cho.ItemDimension SELECT
+    gen_random_uuid(),
+    i.ItemCategoria,
+    i.ItemNome
+FROM cho.Itens i;
+
+-- Inserindo o Calendário
+INSERT INTO dw_cho.CalendarDimension SELECT
+    gen_random_uuid(),
+    d.Dia,
+    d.DataCompleta,
+    d.DiaSemana,
+    d.Mes,
+    d.Trimestre,
+    d.Ano
+FROM (
+    SELECT
+        EXTRACT(DAY FROM p.PedidoData) AS Dia,
+        CAST(p.PedidoData AS DATE) AS DataCompleta,
+        CAST(TO_CHAR(p.PedidoData, 'DY') AS WEEKDAY) AS DiaSemana,
+        EXTRACT(MONTH FROM p.PedidoData) AS Mes,
+        CAST(TO_CHAR(p.PedidoData, 'Q') AS INT) AS Trimestre,
+        EXTRACT(YEAR FROM p.PedidoData) AS Ano
+    FROM cho.Pedidos p
+) d
+WHERE CAST(d.DataCompleta AS DATE) NOT IN (SELECT DataCompleta FROM dw_cho.CalendarDimension);
+
+--   CalendarKey TEXT NOT NULL,
+--   Dia INT NOT NULL CHECK (Dia BETWEEN 1 AND 31),
+--   DataCompleta DATE NOT NULL,
+--   DiaSemana WEEKDAY NOT NULL,
+--   Mes INT NOT NULL CHECK (Mes BETWEEN 1 AND 12),
+--   Trimestre INT NOT NULL CHECK (Trimestre BETWEEN 1 AND 4),
+--   Ano INT NOT NULL,
+--   PRIMARY KEY (CalendarKey)
