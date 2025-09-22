3d1300c96f6a6a77aa67c8e1721db632e86c531f
ALEX JÚNIO MAIA DE OLIVEIRA <B435917@fgv.edu.br>
2025-09-22 17:41:26 -0300

    Organizando os arquivos sql

---
+++ sql/DROP_TABLES_DW_CHO.sql
@@ -0,0 +1,11 @@
+\c cho
+
+SET search_path=dw_cho;
+
+DROP TABLE ReceitaFato;
+DROP TABLE ClienteDimension;
+DROP TABLE FilialDimension;
+DROP TABLE ItemDimension;
+DROP TABLE CalendarDimension;
+
+\c postgres
